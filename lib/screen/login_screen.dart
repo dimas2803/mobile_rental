c391dc0d4e2a396477967447eedc760f25c19db3
FlutLab.io <>
2022-06-06 14:14:36 +0000

    Autosave

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:uas/screen/home_screen.dart';
+import 'package:uas/screen/signub_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
