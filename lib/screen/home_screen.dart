50cfa184fdfde74c0a04ba0a82a48d2c8e160414
FlutLab.io <>
2022-06-20 09:20:30 +0000

    Autosave

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.blue,
         title: Text(
           "Home Screen",
           style: TextStyle(color: Colors.black),
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -14,7 +14,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   startTimer() async {
-    var duration = Duration(seconds: 10);
+    var duration = Duration(seconds: 5);
     return Timer(duration, () async {
       var box = Hive.box('userBox');
       bool? sudahLogin = box.get('isLogin');
