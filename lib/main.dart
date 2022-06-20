47f098f9cb99abcda827bfc901fe225ba75111e3
FlutLab.io <>
2022-06-20 16:25:52 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:uas/my_drawer_header.dart';
 import 'package:uas/screen/home_screen.dart';
 import 'package:uas/screen/splash_screen.dart';
-import 'package:uas/my_drawer_header.dart';
-import 'package:uas/widget/dashboard.dart';
-import 'package:uas/widget/contacts.dart';
-import 'package:uas/widget/events.dart';
-import 'package:uas/widget/notes.dart';
-import 'package:uas/widget/notifications.dart';
-import 'package:uas/widget/privacy_policy.dart';
-import 'package:uas/widget/send_feedback.dart';
-import 'package:uas/widget/settings.dart';
 
 void main() async {
   await Hive.initFlutter();
