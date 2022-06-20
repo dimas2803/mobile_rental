e9eaaf6f8268842e8181a11fefd7819d8070875a
FlutLab.io <>
2022-06-20 11:25:37 +0000

    Autosave

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -94,26 +94,6 @@ class HomeScreen extends StatelessWidget {
         Center(
           child: Text('RENTAL MOBILKU', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
         ),
-        SizedBox(height: 20),
-        SizedBox(
-          width: 250,
-          height: 50,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-            onPressed: () {
-              var box = Hive.box('userBox').clear();
-
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => LoginScreen()),
-              );
-            },
-            child: Text('Logout'),
-          ),
-        ),
       ]),
     );
   }
