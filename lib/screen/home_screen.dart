1e4e3235009110b5b76bc090330e9fac53ad0ad2
FlutLab.io <>
2022-06-20 09:18:45 +0000

    Autosave

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,16 +12,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: new IconButton(
-            icon: Icon(Icons.account_circle),
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => LoginScreen()),
-              );
-            }),
-        iconTheme: IconThemeData(
-          color: Colors.black, //change your color here
-        ),
         backgroundColor: Colors.transparent,
         title: Text(
           "Home Screen",
