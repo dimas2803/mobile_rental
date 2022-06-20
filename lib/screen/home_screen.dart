e7f5cb66884138fa5eeaa6be3e46fb2cee14d03d
FlutLab.io <>
2022-06-20 08:24:29 +0000

    Autosave

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,8 +9,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
+        leading: new IconButton(
+            icon: Icon(Icons.account_circle),
             onPressed: () {
               Navigator.of(context).pop();
             }),
