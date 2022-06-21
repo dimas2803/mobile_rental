76e1e10ceda5fa797d74cb4c1b8f4cc3eb56ceb5
FlutLab.io <>
2022-06-21 07:59:18 +0000

    Autosave

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF0D55CD);
 const textColor = Color(0xFF282A3E);
-const backgroundColor = Color(0xFFF9F7FE);
+const backgroundColor = Colors.teal[600];
