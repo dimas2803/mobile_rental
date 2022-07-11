a326431f8c4db4b289ca608092d3f1f35b8224d3
FlutLab.io <>
2022-07-11 03:20:07 +0000

    Autosave

--- lib/screen/detail/detail_mobil.dart
+++ lib/screen/detail/detail_mobil.dart
@@ -1,234 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:uas/screen/list_car.dart';
 
-class Booking extends StatefulWidget {
-  @override
-  _Booking createState() => _Booking();
+class DetailMobil extends StatefulWidget {
+  const DetailMobil({Key key}) : super(key: key);
+  _DetailMobilState createState() => _DetailMobilState();
 }
 
-class _Booking extends State<Booking> {
-  var _index = 0;
-  bool isDisabled = true;
-
-  Widget _builderStep() => Container(
-        margin: EdgeInsets.only(top: 20),
-        color: Colors.blueGrey,
-        child: Stepper(
-          steps: [
-            Step(
-              title: Text("Data Penerima"),
-              content: new Container(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Penerima',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Email',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.number,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Nomer Hp',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    RaisedButton(
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.green,
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Step(
-              title: Text("Alamat Penerima"),
-              content: new Container(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Alamat',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Desa',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Kecamatan',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Kabupaten/Kota',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.number,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Provinsi',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    RaisedButton(
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.green,
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Step(
-                title: Text("Booking"),
-                content: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(children: [
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Waktu Pengambilan',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 12),
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white70, width: 1.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black87, width: 1.0),
-                        ),
-                        hintText: 'Total Hari Booking',
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    RaisedButton(
-                      child: Text(
-                        "BAYAR",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      color: Colors.green,
-                      onPressed: () {},
-                    ),
-                  ]),
-                )),
-          ],
-          currentStep: _index,
-          onStepTapped: (index) {
-            setState(() {
-              _index = index;
-            });
-          },
-          controlsBuilder: (BuildContext context, {VoidCallback onStepContinue, VoidCallback onStepCancel}) => Container(),
-        ),
-      );
-
+class _DetailMobilState extends State<DetailMobil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: new Text('Booking'),
-          backgroundColor: Color(0xFF203e5a),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => ListCar(),
+            ));
+          },
         ),
-        backgroundColor: Colors.blueGrey,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              _builderStep()
-            ],
-          ),
-        ));
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.teal[700],
+        centerTitle: true,
+        title: Text(
+          "Detail Mobil",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Text("Ini Adalah Halaman Detail Mobil"),
+      ),
+    );
   }
 }
